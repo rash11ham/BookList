71c153d76d686d9d0f2ec6f2262d475bc92305eb
RashedElam <rashedelham3@gmail.com>
2024-03-08 10:27:13 +1100

    intial commit

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required] // name property can not be empty
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
+}
